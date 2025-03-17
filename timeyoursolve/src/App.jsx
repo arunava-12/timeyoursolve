a56adee6ab7a97f3c91c96dbd460c4c426d8a2bf
Jagannath Saha <hello.jagannathsaha@gmail.com>
2025-03-17 14:13:34 +0530

    navbar

--- timeyoursolve/src/App.jsx
+++ timeyoursolve/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import Navbar from "./components/NavBar";
 
 function App() {
-
   return (
-   <div>
-    Hello
-   </div>
-  )
+    <div>
+      <Navbar />
+      <main className="p-4 text-center">
+        <h2 className="text-xl">Scramble and Timer will go here!</h2>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ timeyoursolve/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+      <nav className="bg-blue-600 text-white py-4 px-6 flex justify-between items-center shadow-md">
+        <h1 className="text-2xl font-bold">Cube Timer</h1>
+        <div className="text-xl cursor-pointer">⚙️</div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
